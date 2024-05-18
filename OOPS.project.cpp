4b023eb20a212f2e3678a71b2d3c47b7b2f2ddd5
vyshnavpradeep2004 <vyshnav2004@ug.cusat.ac.in>
2024-05-18 14:01:19 +0530

    Update OOPS.project.cpp

--- OOPS.project.cpp
+++ OOPS.project.cpp
@@ -26,46 +26,64 @@ void medicines_accessories::drops()
   cout<< "Enter the field:";
   cin>>field;
 }
+
+
 void medicines_accessories::tablets()
 {
   cout<< "Enter the field:";
   cin>>field;
 }
+
+
 void medicines_accessories::suppositories()
 {
   cout<< "Enter the field:";
   cin>>field;
 }
+
+
 void medicines_accessories::capsules()
 {
   cout<< "Enter the field:";
   cin>>field;
 }
+
+
 void medicines_accessories::syrups()
 {
   cout<< "Enter the field:";
   cin>>field;
 }
+
+
 void medicines_accessories::injections()
 {
   cout<< "Enter the field:";
   cin>>field;
 }
+
+
 void medicines_accessories::inhalers()
 {
   cout<< "Enter the field:";
   cin>>field;
 }
+
+
 void medicines_accessories::patches()
 {
   cout<< "Enter the field:";
   cin>>field;
 }
+
+
 void medicines_accessories::buccal_tablets()
 {
   cout<< "Enter the field:";
   cin>>field;
 }
+
+
 void medicines_accessories::medical_accessories()
 {
   cout<< "Enter the field:";
